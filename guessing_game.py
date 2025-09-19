db9122a6c8bd8cf3096aa9fb5c6037c89f4d8200
Jace Barrett <d00434672@utahtech.edu>
2025-09-19 13:41:45 -0600

    added easy and hard mode to the game

--- guessing_game.py
+++ guessing_game.py
@@ -1,33 +1,65 @@
 import random
 
 # generate random number
-lucky_number = random.randint(1,100)
+
 
 # Set up guesses and attempts
 max_guesses = 10
 guesses_taken = 0
 
-# Start Game loop
+#Have user choose which mode they want to play
+user_mode = int(input("Press '1' for easy mode, and '2' for hard mode. "))
 
-while guesses_taken < max_guesses:
-    try:
-        # Get user's guess
-        guess = int(input("Guess a number between 1 and 100. \n"))
-    except:
-        print("Invalid input. Please enter a number.")
-        continue
-    
-    guesses_taken += 1
+# easy mode
+if user_mode == 1:
+    lucky_number = random.randint(1,10)
+    # Start Game loop
+    while guesses_taken < max_guesses:
+        try:
+            # Get user's guess
+            guess = int(input("Guess a number between 1 and 10. \n"))
+        except:
+            print("Invalid input. Please enter a number.")
+            continue
+        
+        guesses_taken += 1
 
-    # Checks for a win
-    if guess < lucky_number:
-        print("Too low!")
-    elif guess > lucky_number:
-        print("Too high!")
-    else:
-        print(f"Good job! You guessed the lucky number in {guesses_taken} guesses!")
-        break
+        # Checks for a win
+        if guess < lucky_number:
+            print("Too low!")
+        elif guess > lucky_number:
+            print("Too high!")
+        else:
+            print(f"Good job! You guessed the lucky number in {guesses_taken} guesses!")
+            break
 
-    # If the user runs out of guesses
-    if guess != lucky_number and guesses_taken == 10:
-        print(f"You ran out of guesses! Game over.")
+        # If the user runs out of guesses
+        if guess != lucky_number and guesses_taken == 5:
+            print(f"You ran out of guesses! Game over.")
+
+#hard mode
+if user_mode == 2:
+    lucky_number = random.randint(1,100)
+    # Start Game loop
+    while guesses_taken < max_guesses:
+        try:
+            # Get user's guess
+            guess = int(input("Guess a number between 1 and 100. \n"))
+        except:
+            print("Invalid input. Please enter a number.")
+            continue
+        
+        guesses_taken += 1
+
+        # Checks for a win
+        if guess < lucky_number:
+            print("Too low!")
+        elif guess > lucky_number:
+            print("Too high!")
+        else:
+            print(f"Good job! You guessed the lucky number in {guesses_taken} guesses!")
+            break
+
+        # If the user runs out of guesses
+        if guess != lucky_number and guesses_taken == 10:
+            print(f"You ran out of guesses! Game over.")
